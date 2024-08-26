b5a1704209c4b5605ef92e40ff14d2c7bcf9513f
meranhan25 <meranhany25@gmail.com>
2024-08-27 02:51:49 +0300

    adding the constatns like images and inputs

---
+++ Frontend/src/Constatnts/InputsFields.jsx
@@ -0,0 +1,8 @@
+// Const Inputs
+export const inputFields = [
+    { label: "Name", type: "text", name: "name", placeholder: "Name", autoComplete: "name" },
+    { label: "Email", type: "email", name: "email", placeholder: "Email", autoComplete: "email" },
+    { label: "Phone", type: "number", name: "phone", placeholder: "Phone", autoComplete: "mobile tel" },
+    { label: "Password", type: "password", name: "password", placeholder: "Password", autoComplete: "current-password" },
+    { label: "Confirm Password", type: "password", name: "password_confirmation", placeholder: "Confirm Password", autoComplete: "current-password" },
+];
